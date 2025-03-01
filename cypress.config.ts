8b877350efaa77cdb4923cbdbc52b6510c970165
danadajian <danadajian@gmail.com>
2025-03-01 14:14:28 -0600

    fix cypress tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,36 @@
-import { defineConfig } from 'cypress';
+import { defineConfig as defineCypressConfig } from 'cypress';
+import { defineConfig as defineViteConfig } from 'vite';
 
-export default defineConfig({
+const development = process.env.NODE_ENV === 'development';
+const extensions = ['.web.tsx', '.tsx', '.web.ts', '.ts', '.web.jsx', '.jsx', '.web.js', '.js', '.css', '.json', '.mjs'];
+
+export default defineCypressConfig({
   component: {
     devServer: {
       framework: 'react',
-      bundler: 'webpack'
+      bundler: 'vite',
+      viteConfig: defineViteConfig({
+        clearScreen: true,
+        define: {
+          global: 'window',
+          __DEV__: JSON.stringify(development),
+          DEV: JSON.stringify(development),
+          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        },
+        resolve: {
+          extensions: extensions,
+          alias: {
+            'react-native': 'react-native-web'
+          }
+        },
+        optimizeDeps: {
+          esbuildOptions: {
+            resolveExtensions: extensions,
+            jsx: 'automatic',
+            loader: { '.js': 'jsx' }
+          }
+        }
+      })
     }
   },
   screenshotOnRunFailure: false,
