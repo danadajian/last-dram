c262f663468cb268aea084c46c3cffe903430b35
danadajian <danadajian@gmail.com>
2025-03-09 17:12:54 -0500

    fix db?

--- src/backend/db.ts
+++ src/backend/db.ts
@@ -1,7 +1,7 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import { postgres } from 'bun';
+import { drizzle } from 'drizzle-orm/bun-sql';
+import { SQL } from 'bun';
 import { environmentVariables } from './env';
 
-const client = postgres(environmentVariables.POSTGRES_URL, { max: 100, idleTimeout: 20, maxLifetime: 60 * 30 });
+const client = new SQL(environmentVariables.POSTGRES_URL, { max: 100, idleTimeout: 20, maxLifetime: 60 * 30 });
 
 export const db = drizzle({ client });
