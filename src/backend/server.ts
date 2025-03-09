7c33da4da1395a2d76bf3a8314e8b72534767ccf
danadajian <danadajian@gmail.com>
2025-03-09 16:11:53 -0500

    simplify server

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -2,23 +2,10 @@ import { createBunServeHandler } from 'trpc-bun-adapter';
 import { router } from './router';
 
 const server = Bun.serve({
-  ...createBunServeHandler({
-    router,
-    responseMeta() {
-      return {
-        status: 200,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-          'Access-Control-Allow-Headers': 'Content-Type, Authorization'
-        }
-      };
-    }
-  }),
   routes: {
     '/health': new Response('healthy')
   },
-  port: 'PORT' in process.env ? Number(process.env.PORT) : 8080
+  ...createBunServeHandler({ router })
 });
 
 console.log(`Server running at ${server.url}`);
