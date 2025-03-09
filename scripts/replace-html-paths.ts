2c16f1ae53b39e6c55b78e39bc00a2470d65e1cc
danadajian <danadajian@gmail.com>
2025-03-08 21:37:32 -0600

    public directory?

--- scripts/replace-html-paths.ts
+++ scripts/replace-html-paths.ts
@@ -1,6 +1,10 @@
+import { $ } from 'bun';
+
 const html = await Bun.file('./dist/index.html').text();
 const replacedHtml = html.replace(/="\//g, '="./');
-await Bun.write('./dist/index.html', replacedHtml);
+await Bun.write('./public/index.html', replacedHtml);
 console.log('Updated index.html to set relative paths');
+
+await $`cp -r dist public`;
 
 export {};
