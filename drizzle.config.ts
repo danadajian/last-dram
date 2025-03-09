61a0dc00b503b41072c27c0e2ce20bcfc2419f75
danadajian <danadajian@gmail.com>
2025-03-09 17:04:42 -0500

    fix

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit';
 import { environmentVariables } from './src/backend/env';
 
 export default defineConfig({
-  schema: './src/backend/schema',
+  schema: './src/backend/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
     url: environmentVariables.POSTGRES_URL
