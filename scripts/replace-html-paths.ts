288236f4889abc8ef1a3bff673e569576c5586e5
danadajian <danadajian@gmail.com>
2025-03-08 20:48:43 -0600

    add logging

--- scripts/replace-html-paths.ts
+++ scripts/replace-html-paths.ts
@@ -1,5 +1,6 @@
 const html = await Bun.file('./dist/index.html').text();
 const replacedHtml = html.replace(/="\//g, '="./');
 await Bun.write('./dist/index.html', replacedHtml);
+console.log('Updated index.html to set relative paths');
 
 export {};
