7766fa8806110415671adf28b3bc595e5fe471c5
danadajian <danadajian@gmail.com>
2025-03-10 09:05:10 -0500

    update tables

--- src/backend/schema.ts
+++ src/backend/schema.ts
@@ -1,17 +1,17 @@
 import { decimal, pgTable, uuid, varchar, timestamp } from 'drizzle-orm/pg-core';
 
-export const collections = pgTable('collections', {
-  id: uuid('id').primaryKey().defaultRandom(),
-  ownerFirstName: varchar('ownerFirstName', { length: 256 }).notNull(),
-  ownerLastName: varchar('ownerLastName', { length: 256 }).notNull(),
-  createdAt: timestamp('createdAt').notNull().defaultNow()
-});
-
 export const bottles = pgTable('bottles', {
   id: uuid('id').primaryKey().defaultRandom(),
   name: varchar('name', { length: 256 }).notNull(),
   type: varchar('type', { length: 256 }).notNull(),
   size: varchar('type', { length: 256 }).notNull(),
   proof: decimal().notNull(),
+  createdAt: timestamp('createdAt').notNull().defaultNow()
+});
+
+export const collectedBottles = pgTable('collectedBottles', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  bottleId: uuid('bottleId').notNull(),
+  userId: uuid('userId').notNull(),
   createdAt: timestamp('createdAt').notNull().defaultNow()
 });
